687e77c26b019715b86cd5c021325968e455d496
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-11-16 17:39:33 -0800

    adding markers

--- crisisbot/src/App.js
+++ crisisbot/src/App.js
@@ -1,34 +1,75 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { InfoWindow, Marker } from 'google-maps-react';
+import axios from 'axios';
 
-// const result = dotenv.config()
+// gets info from api
+
+// map information
 
 const mapStyles = {
   width: '100%',
-  height: '100%'
+  height: '100%',
+  position: 'relative'
 };
 
+
 export class MapContainer extends Component {
+
     state = {
-     showingInfoWindow: false,  //Hides or the shows the infoWindow
-     activeMarker: {},          //Shows the active marker upon click
-     selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
-   };
-  render() {
-      console.log(process.env.BLABLA);
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-      />
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+
+    componentDidMount() {
+      axios.get(`https://cors-anywhere.herokuapp.com/https://crisis-beacon-server.herokuapp.com/beacons`).then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            items: result.data
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
     );
-  }
+}
+
+  render() {
+
+      // api call
+      const { error, isLoaded, items } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+
+            <Map
+              google={this.props.google}
+              zoom={14}
+              style={mapStyles}
+            >
+                for (var i = 0; i < items.length; i++) {
+                <Marker position={{lat: {item[i].latitude}, lng: {item[i].longitude}}} />
+                }
+
+            </Map>
+          // <ul>
+          //   {items.map(item => (
+          //     <li key={item.username}>
+          //       {item.username}: {item.name}
+          //     </li>
+          //   ))}
+          // </ul>
+        );
+      }
+}
 }
 
 export default GoogleApiWrapper({
